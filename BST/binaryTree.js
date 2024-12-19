8f12135404c50e1d49fc36e721073fb96786ab1c
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-19 11:00:42 +0530

    chnges

--- BST/binaryTree.js
+++ BST/binaryTree.js
@@ -1,10 +1,11 @@
-class Node {
-    constructor(data) {
+class Node{
+    constructor(data){
         this.data = data
         this.left = null
         this.right = null
     }
 }
+
 
 class Tree{
     constructor(){
@@ -15,64 +16,66 @@ class Tree{
         const newNode = new Node(data)
 
         if(this.root==null){
-            this.root = newNode 
+            this.root = newNode
             return
         }
 
         let temp = this.root
 
-        while(true){
+        
 
-            if(newNode.data==temp.data){
+        while(true){
+            if(newNode.data == temp.data){
                 return undefined
             }
 
-            if(newNode.data < temp.data){
-                if(temp.left==null){
-                    temp.left = newNode
-                    return
-                }else{
-                    temp = temp.left
-                }
-            }
-
             if(newNode.data>temp.data){
-                if(temp.right == null){
+                if(temp.right===null){
                     temp.right = newNode
-                    return
                 }else{
                     temp = temp.right
                 }
+            }else if(newNode.data<temp.data){
+                if(temp.left==null){
+                    temp.left = newNode
+                }else{
+                    temp = temp.left  
+                }
             }
-
         }
-
     }
 
     includes(value){
-        if(this.root==null){
+
+        if (this.root==null){
             return undefined
         }
-        let k = 0
         let temp = this.root
+
         while(temp){
-            if(value > temp.data){
-                temp = temp.right
-            }else if(value < temp.data){
-                temp = temp.left
-            }else if(temp.data==value){
+            if(value==temp.data){
                 return true
+            }else if(value > temp.data){
+                temp = temp.right
+            }else if(value<temp.data){
+                temp = temp.left
             }
         }
+
         return false
+
     }
+
+
 }
 
-const tree = new Tree()
 
-tree.insert(10)
+
+
+const tree = new Tree()
 tree.insert(20)
 tree.insert(30)
-tree.insert(40)
+tree.insert(10)
+tree.insert(8)
+console.log(tree.includes(10));
 console.log(tree)
-console.log(tree.includes(50))
