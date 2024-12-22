9acb3b8080e82c255f3a08d5407919a1dd51f274
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-22 13:06:18 +0530

    changes

---
+++ Trie/trie.js
@@ -0,0 +1,16 @@
+class TrieNode{
+    constructor(){
+        this.children = {}
+        isEndoFword = false
+    }
+}
+
+class Trie{
+    constructor(){
+        this.root = new TrieNode()
+    }
+
+    insert(word){
+        
+    }
+}
