6d9dfe4a74840d88b354e9cb117c25b2a64f41ab
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-22 14:53:32 +0530

    changes

--- Trie/trie.js
+++ Trie/trie.js
@@ -22,10 +22,35 @@ class Trie{
         newNode.isEnd = true
     }
 
+    search(word){
+        let newNode = this.root
+        for(let char of word){
+            if(!newNode.childrenNode[char]){
+                return false
+            }
+            newNode = newNode.childrenNode[char]
+        }
+        return newNode.isEnd
+    }
+
+    produce(word){
+        let newNode = this.root
+        let arr = []
+        for(let char of word){
+            if(!newNode.childrenNode[char]){
+                return false
+            }
+            arr.push(newNode.childrenNode[char])
+            newNode = newNode.childrenNode[char]
+        }
+        return arr
+        return newNode.childrenNode
+    }
 
 }
 
 const trie = new Trie()
 trie.insert('hel')
-trie.insert('hero')
-console.dir(trie,{depth:null})
+console.log(trie.produce('he'))
+// console.log(trie.search("hel"))
+// console.dir(trie,{depth:null})
