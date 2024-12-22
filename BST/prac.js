d00df34ec89d320f69e417eea7bf32f29e7742bc
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-22 09:28:11 +0530

    chnges

--- BST/prac.js
+++ BST/prac.js
@@ -1,3 +1,53 @@
-const graph = {
-    a:[b,c]
+class Node{
+    constructor(data){
+        this.data = data
+        this.left = null
+        this.right = null
+    }
 }
+
+class Tree{
+    constructor(){
+        this.root = null
+    }
+
+    insert(data){
+        const newNode = new Node(data)
+        if(this.root == null){
+            this.root =newNode
+            return 
+        }
+        let current = this.root
+        while(true){
+
+            if(current.data == newNode.data){
+                return
+            }
+
+            if(newNode.data > current.data){
+                if(current.right==null){
+                    current.right = newNode   
+                }else{
+                    current =  current.right
+                }
+            }else{
+                if(current.left==null){
+                    current.left = newNode   
+                }else{
+                    current =  current.left
+                }
+            }
+        }
+
+    }
+
+    get(data){
+        
+    }
+
+}
+
+const tree = new Tree()
+ tree.insert(10)
+ tree.insert(22)
+console.log(tree)
