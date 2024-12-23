c05d6f7ee087879e650278dbdfd0f7f7983856d6
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-23 08:46:24 +0530

    changes

--- Trie/trie.js
+++ Trie/trie.js
@@ -33,34 +33,32 @@ class Trie{
         return newNode.isEnd
     }
 
-    autocomplete(prefix) {
-        let newNode = this.root;
-        // Traverse to the node corresponding to the prefix
-        for (let char of prefix) {
-            if (!newNode.childrenNode[char]) {
-                return []; // Prefix not found
+   autocomplete(prefix){
+        let newNode = this.root
+        for(let char of prefix){
+            if(!newNode.childrenNode[char]){
+                return 
             }
-            newNode = newNode.childrenNode[char];
+            newNode = newNode.childrenNode[char]
         }
-        // Helper function to collect all words from the current node
-        const words = [];
-        const collectWords = (node, path) => {
-            if (node.isEnd) {
-                words.push(path); // Add the word to the list
-            }
-            for (let char in node.childrenNode) {
-                collectWords(node.childrenNode[char], path + char);
-            }
-        };
 
-        // Collect words starting from the prefix node
-        collectWords(newNode, prefix);
-        return words;
-    }
+        let words=[]
+        function collectwords(node,prefix){
+            if(node.isEnd){
+                words.push(prefix)
+            }
+            for(let char in node.childrenNode){
+                collectwords(node.childrenNode[char],prefix+char)
+            }
+        }
+        collectwords(newNode,prefix)
+        return words
+   }
 }
 
 const trie = new Trie()
 trie.insert('hel')
+console.log(trie.autocomplete('h'))
 
 // console.log(trie.search("hel"))
 // console.dir(trie,{depth:null})
