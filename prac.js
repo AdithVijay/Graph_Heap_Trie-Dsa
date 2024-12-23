adaf8138ce8615a5154045c23d1a29e33e178eab
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-23 15:22:29 +0530

    cahnges

--- prac.js
+++ prac.js
@@ -1,48 +1,11 @@
-class Graph{
-    constructor(){
-           this.adjacencyList = {}
+class Tree{
+    constructor(data){
+        this.data = data
+        this.left = null
+        this.right = null
     }
 
-    addVertex(vtx){
-        if(!this.adjacencyList[vtx]){
-            this.adjacencyList[vtx] =[]
-        }
-    }
-
-    adEdge(vtx1,vtx2){
-        if(this.adjacencyList[vtx1] &&this.adjacencyList[vtx2]){
-            this.adjacencyList[vtx1].push(vtx2)
-            this.adjacencyList[vtx2].push(vtx1)
-        }
-    }
-
-    bfs(start){
-        let queue = [start]
-        let visited = []
-        let result = []
-
-        visited.push(start)
-
-        while(queue.length>0){
-            let current = queue.pop()
-            result.push(current)
-            for(let neighbour of this.adjacencyList[current]){
-                if(!visited.includes(neighbour)){
-                    queue.push(neighbour)
-                    visited.push(neighbour)
-                }
-            }
-        }
-        return result
+    insert(){
+        
     }
 }
-
-const g = new Graph()
-g.addVertex('A')
-g.addVertex('B')
-g.addVertex('C')
-g.adEdge('A','B')
-g.adEdge('A','C')
-g.adEdge('B','C')
-console.log(g.bfs('A'))
-console.log(g);
